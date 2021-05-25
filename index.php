2594a70e2312b2ea048c08de22904ff899c0ec90
user.email <adam.thomascosta@gmail.com>
2021-05-25 16:14:07 -0300

    Atualização Estudo

--- index.php
+++ index.php
@@ -1,6 +1,26 @@
 <?php
-
-    $nome = "Thomas";
-    echo "Primeira Página, bem vindo!";
-
+$nome = "Thomas Adam da Costa"
 ?>
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Estudo de PHP</title>
+</head>
+
+<body>
+    <h1>
+        Primeira Página
+    </h1>
+    <br />
+    <h2> 
+        <?php 
+            echo $nome; 
+        ?> 
+    </h2>
+</body>
+
+</html>
